bacdd4e5f14c5845c5e203057caf21f73ba76669
Abhrajit Chatterjee <abhrajitc23@gmail.com>
2025-06-14 23:19:01 +0530

    Added role type for the users

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     name: String,
     email: { type: String, unique: true },
     password: String,
-    isHost: { type: Boolean, default: false }
+    isHost: { type: Boolean, default: false },
+    role: { type: String, enum: ["user", "host"], default: "user" }
 });
 
 module.exports = mongoose.model("User", UserSchema);
